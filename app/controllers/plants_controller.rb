6b9c37f1902c17f3503def38f3544e278ff904c7
Hoang Tran <jashoang2583@gmail.com>
2023-01-10 13:35:41 -0800

    commit tree 2fb6e2306bd93025c577250ad30553e54abd61a7

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+  # rescue_from ActiveRecord::RecordNotFound, with :render_not_found
   # GET /plants
   def index
     plants = Plant.all
@@ -18,7 +18,27 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update 
+    plant = find_plant 
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy 
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
+
+  def find_plant 
+    Plant.find_by(id: params[:id])
+  end
+
+  def render_not_found 
+    render json: { error: "Plant not found" }, status: :not_found
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
